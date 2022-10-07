39d8f7dea8af9ee3a2f271a5dcdfc4faa702dca3
PRamsey02 <prestonkramsey02@icloud.com>
2022-10-07 01:07:51 -0500

    calendar schedule, js current day display

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+// User Story:
+// AS AN employee with a busy schedule
+// I WANT to add important events to a daily planner
+// SO THAT I can manage my time effectively
+
+// Acceptance Criteria:
+// WHEN I open the planner, the current day is displayed at the top of the calendar
+// WHEN I scroll down, I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day, each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock, I can enter an event
+// WHEN I click the save button for that timeblock, the text for that event is saved in local storage
+// WHEN I refresh the page, THEN the saved events persist
+
+// Current day is displayed at the top of the calendar
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
